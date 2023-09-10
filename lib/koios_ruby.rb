ab81d624d0ad286507519be6dd141775b0b66182
Robin Böning <robin.boening@gmail.com>
2023-09-10 14:24:48 +0200

    initial commit

---
+++ lib/koios_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "koios_ruby/version"
+
+module KoiosRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/koios_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KoiosRuby
+  VERSION = "0.1.0"
+end
